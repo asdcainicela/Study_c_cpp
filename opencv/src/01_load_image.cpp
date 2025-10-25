b3ef2184c87c54d1ab3ea62ed20ee1553a0de987
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-25 13:25:50 -0500

    Refactor code

--- opencv/src/01_load_image.cpp
+++ opencv/src/01_load_image.cpp
@@ -3,13 +3,20 @@
 
 int main() {
     cv::Mat img = cv::imread("../data/lena.jpg");
+    cv::Mat img2 = cv::imread("../data/fruits.jpg");
 
     if (img.empty()) {
         std::cerr << "No se pudo cargar la imagen" << std::endl;
         return -1;
     }
 
+    if (img2.empty()){
+        std::cerr <<"fallo cargar p mano"<< std::endl;
+        return -1;
+    }
+
     cv::imshow("Imagen cargada", img);
+    cv::imshow("Imagen fruits load p", img2);
     cv::waitKey(0);
     return 0;
 }
