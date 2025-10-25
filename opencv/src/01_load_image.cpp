62cddf9942aadc290a8a0402a1c904c0b701e67e
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-25 13:38:51 -0500

    Refactor clone and move

--- opencv/src/01_load_image.cpp
+++ opencv/src/01_load_image.cpp
@@ -2,21 +2,37 @@
 #include <iostream>
 
 int main() {
-    cv::Mat img = cv::imread("../data/lena.jpg");
-    cv::Mat img2 = cv::imread("../data/fruits.jpg");
+    cv::Mat img_lena = cv::imread("../data/lena.jpg");
+    cv::Mat img_fruits = cv::imread("../data/fruits.jpg");
 
-    if (img.empty()) {
+    if (img_lena.empty()) {
         std::cerr << "No se pudo cargar la imagen" << std::endl;
         return -1;
     }
 
-    if (img2.empty()){
-        std::cerr <<"fallo cargar p mano"<< std::endl;
+    if (img_fruits.empty()) {
+        std::cerr << "Fallo cargar p mano" << std::endl;
         return -1;
     }
 
-    cv::imshow("Imagen cargada", img);
-    cv::imshow("Imagen fruits load p", img2);
-    cv::waitKey(0);
+    cv::Mat img_fruits_clone = img_fruits.clone(); //clonamos la imagen de fruita
+    cv::resize(img_fruits_clone, img_fruits_clone, cv::Size(640,400)); // redimensionamos la imagen clonada
+
+    cv::Mat img_fruits_clone2 = img_fruits_clone.clone(); //clonamos otra vez
+    cv::resize(img_fruits_clone2, img_fruits_clone2, cv::Size(500,640)); // redimensionamos la imagen clonada 2
+
+    cv::imshow("Imagen cargada", img_lena);
+    cv::moveWindow("Imagen cargada", 0, 0);
+
+    cv::imshow("Imagen fruits load p", img_fruits);
+    cv::moveWindow("Imagen fruits load p", 650, 0);
+
+    cv::imshow("Imagen de fruitas clonada", img_fruits_clone);
+    cv::moveWindow("Imagen de fruitas clonada", 0, 450);
+
+    cv::imshow("Imagen de fruitas clonada 2", img_fruits_clone2);
+    cv::moveWindow("Imagen de fruitas clonada 2", 650, 450);
+
+    cv::waitKey(5000); //podemmos hacer cv::waitKey(0);
     return 0;
 }
