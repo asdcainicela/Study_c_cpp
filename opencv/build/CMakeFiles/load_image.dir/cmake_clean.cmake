768cc91d69f34fba3e9c7cafb63d80963e0cd520
asd <asd@example.com>
2025-10-25 15:26:38 +0000

    add build

---
+++ opencv/build/CMakeFiles/load_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/load_image.dir/src/01_load_image.cpp.o"
+  "load_image"
+  "load_image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/load_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
