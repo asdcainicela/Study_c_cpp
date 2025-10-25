65a0edc7bad2e8089c658a07aa449cdfac4bcd31
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-25 14:38:06 -0500

    Add canvas images

---
+++ opencv/src/02_canvas_images.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(){
+    cv::Mat vect;
+    cv::Mat img1 = imread("../data/home.jpg");
+    cv::Mat img2 = imread("../data/board.jpg");
+    cv::Mat img3 = imread("../data/left01.png");
+    cv::Mat img4 = imread("../data/starry_night.jpg");
+    
+    if (img1.empty() || img2.empty() || img3.empty() || img4.empty()){
+        std::cerr << "Error loading images p bro xd" << std::endl;
+        return -1;
+    }
+     // Redimensionamos para que todas tengan el mismo tamaño
+    cv::Size tile_size(320, 240);
+    cv::resize(img1, img1, tile_size);
+    cv::resize(img2, img2, tile_size);
+    cv::resize(img3, img3, tile_size);
+    cv::resize(img4, img4, tile_size);
+
+
+     // Creamos una imagen vacía para el mosaico (2x2)
+    int rows = 2, cols = 2;
+    cv::Mat canvas(tile_size.height * rows, tile_size.width * cols, img1.type(), cv::Scalar(0,0,0));
+
+    // Pegamos cada imagen en su posición
+    img1.copyTo(canvas(Rect(0, 0, tile_size.width, tile_size.height)));
+    img2.copyTo(canvas(Rect(tile_size.width, 0, tile_size.width, tile_size.height)));
+    img3.copyTo(canvas(Rect(0, tile_size.height, tile_size.width, tile_size.height)));
+    img4.copyTo(canvas(Rect(tile_size.width, tile_size.height, tile_size.width, tile_size.height)));
+
+    // Mostramos una sola ventana
+    cv::imshow("Mosaico", canvas);
+    cv::waitKey(0);
+    
+    return 0;
+}
