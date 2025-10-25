cededf1c55f162eeaaa13d80d1b9d66f078eccd0
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-25 15:40:18 -0500

    Fixed

--- opencv/src/03_filtros_images.cpp
+++ opencv/src/03_filtros_images.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main(){
+    
     cv::Mat img1 = cv::imread("../data/board.jpg");
     cv::Size tile_size(320, 240);
     cv::resize(img1, img1, tile_size);
@@ -9,23 +10,10 @@ int main(){
         std::cerr << "Error loading image p bro xd" << std::endl;
         return -1;
     }
-    cv::blur(img1, img1,cv::Size(5,5));
+    cv::blur(img1, img2,cv::Size(5,5)); //cv::blur(src, dst, cv::Size(5,5)); src -> img original, el otro salida
+
     /*
-    cv::Mat img2 = img1.clone();
-
-    a) Blur / GaussianBlur
-
-blur() → suaviza la imagen promedio de los píxeles vecinos (promedio simple).
-
-cv::blur(src, dst, cv::Size(5,5));
-
-
-src → imagen original
-
-dst → imagen de salida
-
-Size(5,5) → tamaño del kernel (vecinos que se promedian)
-
+   
 GaussianBlur() → suaviza con un kernel gaussiano, más natural que blur().
 
 cv::GaussianBlur(src, dst, cv::Size(5,5), 1.5);
@@ -60,6 +48,7 @@ cv::threshold(gray, binary, 128, 255, cv::THRESH_BINARY);
 
 THRESH_BINARY → tipo de binarización*/
     cv::imshow("Imagen Original", img1);
+    cv::imshow("Imagen Suavizada", img2);
     cv::waitKey(0);
     return 0;
 }
