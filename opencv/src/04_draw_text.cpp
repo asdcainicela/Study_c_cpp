1bba2f1205aba13aaa718cb7de65439738c186d4
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-25 19:07:26 -0500

    Add image

--- opencv/src/04_draw_text.cpp
+++ opencv/src/04_draw_text.cpp
@@ -6,12 +6,12 @@ int main(){
     cv::Mat img(720, 1280, CV_8UC3, cv::Scalar(255, 255, 255)); // Imagen en blanco
 
     //cv::line(image, start_point, end_point, color, thickness);
-    cv::line(img, cv::Point(100,100), cv::Point(500,100), cv::Scalar(255,0,0), 5); // Línea azul
+    cv::line(img, cv::Point(100,100), cv::Point(200,100), cv::Scalar(255,0,0), 5); // Línea azul
 
     cv::rectangle(img, cv::Point(350,100), cv::Point(550,300), cv::Scalar(0,200,200), cv::FILLED); // Rectángulo amarillo relleno
     cv::rectangle(img, cv::Point(600,100), cv::Point(800,300), cv::Scalar(0,0,255), 3); // Rectángulo rojo no relleno
 
-    cv::circle(img, cv::Point(300,200), 50, cv::Scalar(255,0,0), 3);
+    cv::circle(img, cv::Point(200,200), 50, cv::Scalar(255,0,0), 3);
     cv::circle(img, cv::Point(700,200), 75, cv::Scalar(0,255,0), cv::FILLED);
 
     //cv::putText(image, text, org, fontFace, fontScale, color, thickness);
@@ -26,6 +26,10 @@ int main(){
     FONT_HERSHEY_COMPLEX
     FONT_HERSHEY_SCRIPT_SIMPLEX
     */
+
+    cv::Mat img1 = cv::imread("../data/board.jpg");
+
+    img1.copyTo(img(cv::Rect(100,400, img1.cols, img1.rows))); // Pegamos la imagen en el canvas
 
     cv::imshow("Canvas", img);
     cv::waitKey(0);
