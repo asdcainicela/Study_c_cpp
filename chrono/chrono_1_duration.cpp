00a5ce88c82558e259e48de7c243785f72831589
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-28 18:07:50 -0500

    Refactor std::chrono::___

--- chrono/chrono_1_duration.cpp
+++ chrono/chrono_1_duration.cpp
@@ -3,6 +3,13 @@
 
 int main() {
     std::chrono::seconds s(5); // 5 segundos
-
+    std::chrono::milliseconds ms(2500); // 2500 milisegundos
+    std::chrono::microseconds us(3000000); // 3000000 microsegundos
     std::cout << "s: " << s.count() << " segundos\n";
+    std::cout << "ms: " << ms.count() << " milisegundos\n";
+    std::cout << "us: " << us.count() << " microsegundos\n";
+    
+    //std::chrono::duration_cast<TARGET>(DURATION_ORIGINAL)
+    auto total = s+std::chrono::duration_cast<std::chrono::seconds>(ms)+std::chrono::duration_cast<std::chrono::seconds>(us);
+    std::cout << "Time total: " << total.count() << " segundos\n";
 }
