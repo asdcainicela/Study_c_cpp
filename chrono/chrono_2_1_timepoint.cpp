e0f845212f24faa63f3446fdcb3b122881c6d576
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-28 21:41:31 -0500

    Add count billon number time

---
+++ chrono/chrono_2_1_timepoint.cpp
@@ -0,0 +1,37 @@
+/*
+std::chrono::time_point<Clock, Duration>
+
+Clock: tipo de reloj (system_clock, steady_clock, etc.)
+Duration: unidad de tiempo (por defecto, nanosegundos)
+
+*/
+
+#include <iostream>
+#include <chrono>
+
+int main() {
+    // Punto de inicio
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    // Vemos cuanto tarda en contar hasta 1 billón
+    int64_t num = 0;
+    while (num < 1'000'000'000'000) {
+        num++;
+    }
+
+    // Punto de fin
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    // Diferencia entre ambos (duración)
+    std::chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "Tiempo transcurrido: " << elapsed_seconds.count() << " segundos\n";
+}
+
+/*
+steady_clock::now() toma una marca de tiempo (no cambia ni retrocede como el reloj del sistema).
+
+Al restar end - start obtienes una duration, es decir, cuánto tiempo pasó.
+
+duration<double> te da un resultado con decimales (más preciso).
+*/
