8d306e75d2eb94db1d276d02c471396fb524d5dd
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-10-28 21:31:03 -0500

    Refactor double total exactly seconds

--- chrono/chrono_1_duration.cpp
+++ chrono/chrono_1_duration.cpp
@@ -12,4 +12,8 @@ int main() {
     //std::chrono::duration_cast<TARGET>(DURATION_ORIGINAL)
     auto total = s+std::chrono::duration_cast<std::chrono::seconds>(ms)+std::chrono::duration_cast<std::chrono::seconds>(us);
     std::cout << "Time total: " << total.count() << " segundos\n";
+
+    std::chrono::duration<double> total_sec = s + ms + us;
+    std::cout << "Total exacto: " << total_sec.count() << " segundos\n";
+
 }
