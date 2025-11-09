e3bb8fe90b9ec616c32a669cd01a79fad29be966
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-09 11:57:28 -0500

    Refactor build_run_templates.sh to improve organization and clarity of compilation sections

--- templates/build_run_templates.sh
+++ templates/build_run_templates.sh
@@ -2,6 +2,7 @@
 
 # Compilar los archivos de templates C++
 
+# Nivel básico e intermedio
 nvcc -std=c++17 01_funciones.cpp         -o 01_funciones
 nvcc -std=c++17 02_clases.cpp            -o 02_clases
 nvcc -std=c++17 03_valores.cpp           -o 03_valores
@@ -9,12 +10,12 @@ nvcc -std=c++17 04_especializacion.cpp   -o 04_especializacion
 nvcc -std=c++17 05_advanced.cpp          -o 05_advanced
 nvcc -std=c++17 06_tipo_valor.cpp        -o 06_tipo_valor
 
+# Ejemplos prácticos
+nvcc -std=c++17 01_example.cpp           -o 01_example
+nvcc -std=c++17 02_example.cpp           -o 02_example
 
-nvcc -std=c++17 01_example.cpp        -o 01_example
-nvcc -std=c++17 02_example.cpp        -o 02_example
-
-# Archivos futuros 07–10 (por agregar)
-# nvcc -std=c++17 07_ejemplo.cpp         -o 07_ejemplo
-# nvcc -std=c++17 08_ejemplo.cpp         -o 08_ejemplo
-# nvcc -std=c++17 09_ejemplo.cpp         -o 09_ejemplo
-# nvcc -std=c++17 10_ejemplo.cpp         -o 10_ejemplo
+# Nivel avanzado
+nvcc -std=c++17 07_punteros_refs.cpp     -o 07_punteros_refs
+nvcc -std=c++20 08_sensor_array.cpp      -o 08_sensor_array
+nvcc -std=c++17 09_policy_filter.cpp     -o 09_policy_filter
+nvcc -std=c++17 10_signal_pipeline.cpp   -o 10_signal_pipeline
