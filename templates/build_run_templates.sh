f0eb0e82ea1cdb60c58c90a5afab7f9e51a25a42
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-09 12:00:04 -0500

    Update build_run_templates.sh to include .exe extensions for compiled files

--- templates/build_run_templates.sh
+++ templates/build_run_templates.sh
@@ -2,20 +2,19 @@
 
 # Compilar los archivos de templates C++
 
-# Nivel básico e intermedio
-nvcc -std=c++17 01_funciones.cpp         -o 01_funciones
-nvcc -std=c++17 02_clases.cpp            -o 02_clases
-nvcc -std=c++17 03_valores.cpp           -o 03_valores
-nvcc -std=c++17 04_especializacion.cpp   -o 04_especializacion
-nvcc -std=c++17 05_advanced.cpp          -o 05_advanced
-nvcc -std=c++17 06_tipo_valor.cpp        -o 06_tipo_valor
+nvcc -std=c++17 01_funciones.cpp         -o 01_funciones.exe
+nvcc -std=c++17 02_clases.cpp            -o 02_clases.exe
+nvcc -std=c++17 03_valores.cpp           -o 03_valores.exe
+nvcc -std=c++17 04_especializacion.cpp   -o 04_especializacion.exe
+nvcc -std=c++17 05_advanced.cpp          -o 05_advanced.exe
+nvcc -std=c++17 06_tipo_valor.cpp        -o 06_tipo_valor.exe
+nvcc -std=c++17 07_traits_sensor.cpp     -o 07_traits_sensor.exe
+nvcc -std=c++17 08_sensor_array.cpp      -o 08_sensor_array.exe
+nvcc -std=c++17 09_policy_filter.cpp     -o 09_policy_filter.exe
+nvcc -std=c++17 10_signal_pipeline.cpp   -o 10_signal_pipeline.exe
+
 
 # Ejemplos prácticos
 nvcc -std=c++17 01_example.cpp           -o 01_example
 nvcc -std=c++17 02_example.cpp           -o 02_example
 
-# Nivel avanzado
-nvcc -std=c++17 07_punteros_refs.cpp     -o 07_punteros_refs
-nvcc -std=c++20 08_sensor_array.cpp      -o 08_sensor_array
-nvcc -std=c++17 09_policy_filter.cpp     -o 09_policy_filter
-nvcc -std=c++17 10_signal_pipeline.cpp   -o 10_signal_pipeline
