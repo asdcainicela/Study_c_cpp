5d00d4a348628ee5fa401b0a1bf9d970de513b97
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-11-09 12:22:04 -0500

    Remove .exe extensions from output files in build_run.sh

--- vector/build_run.sh
+++ vector/build_run.sh
@@ -2,10 +2,10 @@
 
 # Compilar ejemplos de std::vector en C++
 
-nvcc -std=c++17 01_vector_basico.cpp      -o 01_vector_basico.exe
-nvcc -std=c++17 02_vector_modificar.cpp   -o 02_vector_modificar.exe
-nvcc -std=c++17 03_vector_iteradores.cpp  -o 03_vector_iteradores.exe
-nvcc -std=c++17 04_vector_avanzado.cpp    -o 04_vector_avanzado.exe
-nvcc -std=c++17 05_vector_objetos.cpp     -o 05_vector_objetos.exe
+nvcc -std=c++17 01_vector_basico.cpp      -o 01_vector_basico
+nvcc -std=c++17 02_vector_modificar.cpp   -o 02_vector_modificar
+nvcc -std=c++17 03_vector_iteradores.cpp  -o 03_vector_iteradores
+nvcc -std=c++17 04_vector_avanzado.cpp    -o 04_vector_avanzado
+nvcc -std=c++17 05_vector_objetos.cpp     -o 05_vector_objetos
 
 echo "Compilación completada."
