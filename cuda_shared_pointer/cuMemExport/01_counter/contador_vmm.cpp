690466b12042de34572e21f4cb06f2b0dba27124
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-12-09 02:58:41 -0500

    feat: Add C++ examples for chrono, CUDA, modern C++, OpenCV, pointers, references, RTSP, templates, and vectors, and update .gitignore.

--- cuda_shared_pointer/cuMemExport/01_counter/contador_vmm.cpp
+++ cuda_shared_pointer/cuMemExport/01_counter/contador_vmm.cpp
@@ -41,6 +41,7 @@ public:
         prop.type = CU_MEM_ALLOCATION_TYPE_PINNED;
         prop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
         prop.location.id = 0;
+        prop.requestedHandleTypes = CU_MEM_HANDLE_TYPE_POSIX_FILE_DESCRIPTOR;
         
         // Obtener granularidad de asignación
         size_t granularity;
